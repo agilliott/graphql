b41820f336f019c028472ea91dabea6cc5ed9892
Alexandria Gilliott <alexandriagilliott@outlook.com>
2020-08-05 20:17:14 +0100

    intial commit

---
+++ server/src/index.js
@@ -0,0 +1,33 @@
+const { GraphQLServer } = require('graphql-yoga');
+const { PrismaClient } = require('@prisma/client');
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: async (parent, args, context, info) => {
+      return context.prisma.link.findMany();
+    },
+  },
+  Mutation: {
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
+      return newLink;
+    },
+  },
+};
+
+const prisma = new PrismaClient();
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: {
+    prisma,
+  },
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
